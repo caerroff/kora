678d4de1d076c8ac2e78e66a0f140fcc97dd9d41
Thomas MASIN (SAN DE RGOCC) <56608891+caerroff@users.noreply.github.com>
2024-06-13 11:05:23 +0200

    feat(API): cleanup + organise data

--- index.js
+++ index.js
@@ -17,11 +17,15 @@ const API_KEY = process.env.API_KEY
 const httpsServer = https.createServer({key: key, cert: cert }, app);
 
 const axiosHttp = axios.create({
-    baseURL: 'http://api.openweathermap.org/data/2.5/forecast'
+    baseURL: 'https://api.openweathermap.org'
 })
 
+function kelvinToCelsius(temp){
+    return temp - 273.15
+}
 
 app.get('/forecast', async (req, res) => {
+    
     if(req.method !== 'GET') {
         res.status(405)
         res.send('Method Not Allowed')
@@ -36,35 +40,60 @@ app.get('/forecast', async (req, res) => {
 
     const lat = req.query.lat
     const lon = req.query.lon
+
+    // Using var instead of let to be able to get the value outside of the try block
+    var request;
     try{
-        request = await axiosHttp.get('', {
+        request = await axiosHttp.get('/data/2.5/forecast', {
             params: {
                 lat: lat,
                 lon: lon,
                 appid: API_KEY
             }
         })
-        res.send(request)
     }catch(e){
-        switch(e.response.status){
-            case 500:
-                res.status(500)
-                res.send('Internal Server Error')
-                break
-            case 401:
-                res.status(401)
-                res.send('Wrong API Key for '+e.request.path)
-                break
-        }
+        res.status(e.response.status)
+        res.send(
+            {status: e.response.status,
+             message: e.response.data.message
+            }
+        )
     }
+    if(!request){
+        return
+    }
+    
+    let response = {}
+    response.status = request.status
+    response.data = {}
+    response.data.list = {}
+    for(let i = 0; i < request.data.list.length; i++){
+        if(!response.data.list[request.data.list[i].dt_txt.substr(0, 10)]){
+            response.data.list[request.data.list[i].dt_txt.substr(0, 10)] = []
+        }
+        response.data.list[request.data.list[i].dt_txt.substr(0, 10)].push(kelvinToCelsius(request.data.list[i].main.temp))
+    }
+    //calculate average for each day
+    response.data.average = {}
+    for(let key in response.data.list){
+        let sum = 0
+        for(let i = 0; i < response.data.list[key].length; i++){
+            sum += response.data.list[key][i]
+        }
+        if(sum === 0){
+            continue
+        }
+        response.data.average[key] = sum / response.data.list[key].length
+    }
+    res.send(response)
 })
 
 // Launching servers
 
 httpServer.listen(3000, () => {
-    console.log('Listening port 3000...')
+    console.log('Listening port 3000 for HTTP...')
 })
 
 httpsServer.listen(3001, () => {
-    console.log('Listening port 3001...')
+    console.log('Listening port 3001 for HTTPS...')
 })
